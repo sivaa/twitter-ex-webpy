d7687d6f1f105cd3f97f44f375ed1b1655eaeba8
sivaa <siva@sivaa.in>
2013-01-20 00:50:55 +0530

    adding session base

--- twitter/main.py
+++ twitter/main.py
@@ -1,7 +1,18 @@
 import web
 import urls
 
+app = web.application(urls.urls, globals())
+
+
+if web.config.get('_session') is None:
+    session = web.session.Session(app, web.session.DiskStore('sessions'), {'count': 0})
+    web.config._session = session
+else:
+    session = web.config._session
+
+def get_session():
+	return session
+
 if __name__ == "__main__":
-	app = web.application(urls.urls, globals())
 	app.run()
 
