8b41cc62da655bc9a6eaf239662973aacba25a38
sivaa <siva@sivaa.in>
2013-01-20 04:03:17 +0530

    schema update

--- twitter.sql
+++ twitter.sql
@@ -35,7 +35,32 @@ CREATE TABLE `auth` (
 
 LOCK TABLES `auth` WRITE;
 /*!40000 ALTER TABLE `auth` DISABLE KEYS */;
+INSERT INTO `auth` VALUES ('kk','kk'),('ll','ll'),('nn','nn');
 /*!40000 ALTER TABLE `auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `follower`
+--
+
+DROP TABLE IF EXISTS `follower`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `follower` (
+  `followed_by` varchar(45) NOT NULL,
+  `followed_user` varchar(45) NOT NULL,
+  PRIMARY KEY (`followed_by`,`followed_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `follower`
+--
+
+LOCK TABLES `follower` WRITE;
+/*!40000 ALTER TABLE `follower` DISABLE KEYS */;
+INSERT INTO `follower` VALUES ('kk','ll'),('kk','nn'),('ll','kk'),('ll','nn'),('nn','kk'),('nn','ll');
+/*!40000 ALTER TABLE `follower` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -51,7 +76,7 @@ CREATE TABLE `tweet` (
   `text` varchar(141) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -60,7 +85,7 @@ CREATE TABLE `tweet` (
 
 LOCK TABLES `tweet` WRITE;
 /*!40000 ALTER TABLE `tweet` DISABLE KEYS */;
-INSERT INTO `tweet` VALUES (3,NULL,'first tweet','2013-01-19 18:25:33');
+INSERT INTO `tweet` VALUES (3,NULL,'first tweet','2013-01-19 18:25:33'),(4,'kk','Siva','2013-01-19 19:59:53'),(5,'ll','new user tweet','2013-01-19 20:06:06'),(6,'kk','one more from kk','2013-01-19 20:22:42'),(7,'ll','more tweets from ll','2013-01-19 21:06:56');
 /*!40000 ALTER TABLE `tweet` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -73,4 +98,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-01-20  0:04:34
+-- Dump completed on 2013-01-20  4:03:03
